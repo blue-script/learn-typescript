06325fd0e25e7214b949398bc87c7a4be54a3cba
demianov-an <Alexm92@inbox.ru>
2024-01-05 14:11:35 +0600

    typing through interfaces

---
+++ src/state.ts
@@ -0,0 +1,17 @@
+const a = 1
+
+interface IMan {
+  name: string
+  height: number
+}
+
+interface ICar {
+  model: string
+  year: number
+}
+
+let man1: IMan = { name: 'Dima', height: 1.78 }
+let man2 = { name: 'Sasha', height: 1.8 }
+let car: ICar = { model: 'Reno Stepway', year: 2016 }
+
+export default 1
